7a52d8c44adb12c6d88803ac165bd62e9d3fea94
Maciej Fijalkowski <fijall@gmail.com>
2017-05-17 12:52:00 +0200

    finish the script for checking for recurring payments

--- update-members.py
+++ update-members.py
@@ -2,9 +2,10 @@
 """ A script to update members, run from cron
 """
 
-import treq
+import treq, re, time, sys
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks
+from twisted.python import log
 from sqlalchemy import create_engine
 
 from authsys_common.payments import recurring_payment
@@ -14,21 +15,47 @@ from authsys_common import queries as q
 eng = create_engine(get_db_url())
 con = eng.connect()
 
+log.startLogging(sys.stderr)
+
 members_to_update = q.members_to_update(con)
 
+counter = 1
+
+if len(sys.argv) > 1 and sys.argv[1] == '--dry':
+    dry_run = True
+else:
+    dry_run = False
+
 @inlineCallbacks
-def payment_check(res, tp):
-    r = yield res.json()
-    print r
+def payment_check(res, member_id, tp, sum):
+    global counter
+    if not dry_run:
+        r = yield res.json()
+        print r
+        q.payments_write_transaction(con, member_id, "completed", time.time(),
+                r['id'], r['result']['code'], r['result']['description'], sum, tp)
+
+        if re.search("^(000\.000\.|000\.100\.1|000\.[36])", r['result']['code']):
+            q.add_one_month_subscription(con, member_id, tp)
+        else:
+            q.remove_credit_card_token(con, member_id)
+    counter -= 1
+    if counter == 0:
+        reactor.stop()
 
 def schedule(iter):
+    global counter
     try:
         n = iter.next()
+        print "NEXT:", n
     except StopIteration:
+        counter -= 1
+        if counter == 0:
+            reactor.stop()
         return
-    print n
-    recurring_payment(con, n[1][1], n[0], n[1][2], payment_check)
-    #reactor.callLater(0, schedule, iter)
+    counter += 1
+    recurring_payment(con, n[1][1], n[0], n[1][2], payment_check, dry_run=dry_run)
+    reactor.callLater(0, schedule, iter)
 
 reactor.callLater(0, schedule, members_to_update.iteritems())
 reactor.run()
