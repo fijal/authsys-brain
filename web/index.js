9f448dc58e6618057982600c2948ffa316212797
Maciej Fijalkowski <fijall@gmail.com>
2019-05-11 11:06:52 +0200

    maybe

--- web/index.js
+++ web/index.js
@@ -533,7 +533,7 @@ function update_entries()
          name = elem[1] + " (" + elem[0] + ")"
       else
          name = "token: " + elem[0]
-      r = "<li><span class='" + cls + " circle'></span><span class='list-name'><a onclick='show_member_details_from_access_log(\'" + elem[6] + "\'')' href='#'>" + name
+      r = "<li><span class='" + cls + " circle'></span><span class='list-name'><a onclick='show_member_details_from_access_log(\"" + elem[6] + "\")' href='#'>" + name
       r += "</a></span><span class='list-reason'>" + reason + "</span>" + parse_time(entry_time);
       r += "</span></li>"
       return r;
