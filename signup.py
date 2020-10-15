beeed825d530b9f5e1e994601e68f4bde3a38147
Maciej Fijalkowski <fijall@gmail.com>
2020-10-15 13:28:28 +0200

    missing file

---
+++ signup.py
@@ -0,0 +1,8 @@
+
+from txrestapi.resource import APIResource
+
+class SignupManager(APIResource):
+    def __init__(self, arg):
+        APIResource.__init__(self)
+
+    #@methods.GET('^/signup/indemnity$')
