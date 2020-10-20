807d4241ce5c04ac6084a663c806ae5d848d379e
Maciej Fijalkowski <fijall@gmail.com>
2020-10-20 14:32:25 +0200

    update

--- web/index.js
+++ web/index.js
@@ -94,8 +94,15 @@ function update_member_list(filter)
    } else {
       $("#placeholder").html("Loading....");
       connection.session.call('com.members.list', [filter.slice(0, 3)]).then(function (res) {
+         console.log(filter.slice(0, 3));
+         console.log(res);
          global_status.member_list_prefix = filter.slice(0, 3);
-         continuation(res);
+         if (!res) {
+            global_status.member_list = [];
+            $("#placeholder").html("No hits");
+         } else {
+            continuation(res);
+         }
       });
    }
 }
