fca09ccd00cc511c5b50ea63c0380dfaae159736
Maciej Fijalkowski <fijall@gmail.com>
2020-10-20 10:18:49 +0200

    move indemnity to brain

--- signup.py
+++ signup.py
@@ -1,8 +1,48 @@
 
+import main, base64, py, os, time
+from authsys_common.scripts import get_db_url, get_config
+from authsys_common.model import members
+
+from sqlalchemy import select, func
+
 from txrestapi.resource import APIResource
+from txrestapi import methods
+
 
 class SignupManager(APIResource):
     def __init__(self, arg):
         APIResource.__init__(self)
 
-    #@methods.GET('^/signup/indemnity$')
+    @methods.POST('^/signup/submit$')
+    def submit(self, request):
+        main.con.execute(members.insert().values({
+            'name': request.args['name'][0] + " " + request.args['surname'][0],
+            'email': request.args['email'][0],
+            'phone': request.args['phone'][0],
+            'emergency_phone': request.args['emergency-phone'][0],
+            'spam_consent': request.args.get('spam', 'off')[0] == u'on',
+            'id_number': request.args['id_no'][0],
+            'signature_filename': request.args['filename'][0],
+            'show_up_reason': request.args['reason'][0],
+            'timestamp': int(time.time()),
+        }))
+        #thread.start_new_thread(send_email, (request.args['email'],))
+        return py.path.local(__file__).join('..', 'web', 'thankyou.html').read().replace("{{foo}}", request.args['filename'][0])
+
+    @methods.POST('^/signup/upload_signature')
+    def upload_signature(self, request):
+        # pretty sure it's not how you do it....
+        d = request.content.read()
+        prefix = "data:image/png;base64,"
+        assert d.startswith(prefix)
+        store_dir = get_config().get('data', 'store_dir')
+        # invent new filename
+        no = list(main.con.execute(select([func.count(members)])))[0][0]
+        fname = os.path.join(store_dir, "signature_%d.png" % int(no))
+        d = d[len(prefix):]
+        if (len(d) % 4) != 0:
+            d += "=" * (4 - (len(d) % 4))
+        with open(fname, "w") as f:
+            f.write(base64.b64decode(d, " /"))
+        return fname
+
--- web/indemnity.js
+++ web/indemnity.js
@@ -1,78 +1,174 @@
-var streaming = false;
+var signaturePad;
 
-var width = 320; // We will scale the photo width to this
-var height = 0; // This will be computed based on the input stream
+// the URL of the WAMP Router (Crossbar.io)
+//
+/*var wsuri;
+if (document.location.origin == "file://") {
+   wsuri = "ws://127.0.0.1:8080/ws";
 
-var video = null;
-var canvas = null;
-var photo = null;
-var startbutton = null;
-
-function startup() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
-
-    // access video stream from webcam
-    navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: false
-        })
-        // on success, stream it in video tag
-        .then(function(stream) {
-            video.srcObject = stream;
-            video.play();
-        })
-        .catch(function(err) {
-            console.log("An error occurred: " + err);
-        });
-
-    video.addEventListener('canplay', function(ev) {
-        if (!streaming) {
-            height = video.videoHeight / (video.videoWidth / width);
-
-            if (isNaN(height)) {
-                height = width / (4 / 3);
-            }
-
-            video.setAttribute('width', width);
-            video.setAttribute('height', height);
-            canvas.setAttribute('width', width);
-            canvas.setAttribute('height', height);
-            streaming = true;
-        }
-    }, false);
-
-    startbutton.addEventListener('click', function(ev) {
-        takepicture();
-        ev.preventDefault();
-    }, false);
-
-    clearphoto();
+} else {
+   wsuri = (document.location.protocol === "http:" ? "ws:" : "wss:") + "//" +
+               document.location.host.split(":")[0] + ":8080" + "/ws";
 }
 
-function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
 
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
+// the WAMP connection to the Router
+//
+var connection = new autobahn.Connection({
+   url: wsuri,
+   realm: "authsys",
+   max_retries: -1,
+   max_retry_delay: 3,
+});*/
+
+function update_data(r)
+{
+    var no = r;
+    var d = document.location.host;
+    window.location = "http://" + d + "/update?id=" + no;
 }
 
-function takepicture() {
-    var context = canvas.getContext('2d');
-    if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
+function notify_broadcast(r)
+{
+    var no = r[0];
+    var sum = r[1];
+    var d = document.location.host.split(":")[0];
+    window.location = "http://" + d + ":8080/payment.html?id=" + no + "&sum=" + sum;
+}
 
-        var data = canvas.toDataURL('image/png');
-        photo.setAttribute('src', data);
-    } else {
-        clearphoto();
+$(document).ready(function () {
+    var wrapper = document.getElementById("signature-pad"),
+        clearButton = wrapper.querySelector("[data-action=clear]"),
+        saveButton = wrapper.querySelector("[data-action=save]"),
+        canvas = wrapper.querySelector("canvas");
+
+    /*connection.onopen = function(session, details)
+    {
+        session.subscribe('com.payments.notify_broadcast', notify_broadcast);
+        session.subscribe('com.members.update_data_broadcast', update_data);
     }
-}
 
-window.addEventListener('load', startup, false);
+    connection.open();*/
+
+    // Adjust canvas coordinate space taking into account pixel ratio,
+    // to make it look crisp on mobile devices.
+    // This also causes canvas to be cleared.
+    function resizeCanvas() {
+        // When zoomed out to less than 100%, for some very strange reason,
+        // some browsers report devicePixelRatio as less than 1
+        // and only part of the canvas is cleared then.
+        var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        canvas.getContext("2d").scale(ratio, ratio);
+    }
+
+    window.onresize = resizeCanvas;
+    resizeCanvas();
+
+    signaturePad = new SignaturePad(canvas);
+
+    clearButton.addEventListener("click", function (event) {
+        signaturePad.clear();
+    });
+
+    $('.iagree').on('click', function(e){
+      var ia = $(e.target).closest('.iagree')
+      if (ia.hasClass('accepted')) {
+        ia.removeClass('accepted').find('input').prop('checked',false)
+      } else {
+        ia.addClass('accepted').removeClass('error').find('input').prop('checked',true)
+      }
+    })
+
+    $('.spam').on('click', function(e){
+        var ia = $(e.target).closest('.spam').find('input')
+        ia.prop('checked', !ia.prop('checked'))
+    });
+
+    $('#name').on('keydown', function(){
+      $('#name-input').removeClass('error');
+      $("#error").removeClass('show');
+    });
+    $('#surname').on('keydown', function(){
+      $('#surname-input').removeClass('error');
+      $("#error").removeClass('show');
+    });
+    $('#phone').on('keydown', function(){
+      $('#phone-input').removeClass('error');
+      $("#error").removeClass('show');
+    });
+    $("#emergency-phone").on('keydown', function(){
+      $('#emergency-phone-input').removeClass('error');
+      $("#error").removeClass('show');
+    });
+    $('#id_no').on('keydown', function(){
+      $('#id-input').removeClass('error');
+      $("#error").removeClass('show');
+    })
+    $('#email').on('keydown', function(){
+      $('#email-input').removeClass('error');
+      $("#error").removeClass('show');
+    })
+
+});
+
+function save_signature()
+{
+    var preMessage = 'You have errors:<br><br>'
+    var inputs = $("form").find("input");
+    var errors = [];
+
+    $("#error").html("");
+
+    if ($(".iagree input:checkbox:not(:checked)") .length > 0) {
+      $(".iagree input:checkbox:not(:checked)") .parent().addClass('error');
+      errors.push("Please agree to all sections");
+    }
+    if (!inputs[0].value) {
+        errors.push("<a href='#name-input'><i class='icon-up'></i> Please provide your first name.</a>");
+        $("#name-input").addClass('error');
+    }
+    if (!inputs[1].value) {
+        errors.push("<a href='#surname-input'><i class='icon-up'></i> Please provide your surname.</a>");
+        $("#surname-input").addClass('error');
+    }
+    if (!inputs[2].value) {
+        errors.push("<a href='#id-input'><i class='icon-up'></i> Please provide your South African ID number or passport number.</a>");
+        $("#id-input").addClass('error');
+    }
+    if (!$('#email').val()) {
+        errors.push("<a href='#email-input'><i class='icon-up'></i> Please provide your email address.</a>");
+        $("#email-input").addClass('error');
+    }
+    if ($('#email').val().indexOf("@") == -1) {
+        errors.push("<a href='#email-input'><i class='icon-up'></i> Email address must be valid.</a>");
+        $("#email-input").addClass('error');
+    }
+    if (!$('#phone').val()) {
+        errors.push("<a href='#phone-input'><i class='icon-up'></i> Please provide phone number</a>");
+        $("#phone-input").addClass('error');
+    }
+    if (!$('#emergency-phone').val()) {
+        errors.push("<a href='#emergency-phone-input'><i class='icon-up'></i> Please provide emergency phone number</a>");
+        $("#emergency-phone-input").addClass('error');
+    }
+    
+    if (signaturePad.isEmpty()) {
+        errors.push("<a href='#signature-input'><i class='icon-up'></i> Signature cannot be empty.</a>");
+        $("#error").addClass('show');
+    }
+    if (errors.length > 0) {
+      $("#error").addClass('show');
+      $("#error").html(preMessage + errors.join('<br>'))
+      return false;
+    }
+    $("#error").removeClass('show');
+    $("#progress").html("Uploading signature...");
+    $.post("/signup/upload_signature", signaturePad.toDataURL(), function(res) {
+        $("#input-filename")[0].value = res;
+        $("#submit-button")[0].onclick = undefined;
+        $("#submit-button").click();
+    })
+    return false;
+}
