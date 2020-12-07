52ec2b99307ae1e9db7614798ae179571cfeaf60
Maciej Fijalkowski <fijall@gmail.com>
2020-12-07 13:10:57 +0200

    revert the change

--- main.py
+++ main.py
@@ -49,12 +49,14 @@ class VoucherManager(APIResource):
 
         rand = os.urandom(8).encode('hex')
 
+        EAN = barcode.get_barcode_class('code128')
+
         r = con.execute(vouchers.insert().values(fullname=request.args['name'][0].decode('utf8'),
             reason=request.args['reason'][0].decode('utf8'), extra=request.args['extra'][0].decode('utf8'),
             unique_id=rand, used=False, timestamp=int(time.time())))
         no = str(r.lastrowid)
 
-        ean = barcode.get('code128', str(rand))
+        ean = EAN(str(rand))
         fullname = ean.save('/tmp/ean13_barcode')
         svg2png(url=fullname, write_to="/tmp/out.png", dpi=300)
 
