aa55b56cdcd2f1db8e42ffc603a665a59236ffaa
Maciej Fijalkowski <fijall@gmail.com>
2021-06-01 07:22:11 +0200

    small fixes

--- adjust-subscriptions.py
+++ adjust-subscriptions.py
@@ -35,6 +35,7 @@ for member_id, name, sub_type, charge_day in list(con.execute(select([members.c.
     pending = list(con.execute(select([pending_transactions.c.id]).where(
         and_(pending_transactions.c.member_id == member_id, pending_transactions.c.timestamp > day_start))))
     if len(pending) > 0:
+        print("Skipping %s, pending charges" % name)
         continue
     subs = list(con.execute(select([subscriptions.c.start_timestamp, subscriptions.c.end_timestamp]).where(
         and_(subscriptions.c.member_id == member_id,
@@ -43,7 +44,9 @@ for member_id, name, sub_type, charge_day in list(con.execute(select([members.c.
     assert len(subs) == 1, (subs, name)
     extra_subs = list(con.execute(select([subscriptions.c.end_timestamp]).where(
         and_(subscriptions.c.end_timestamp > day_end + 59*60, subscriptions.c.member_id == member_id))))
-    assert len(extra_subs) == 0
+    if len(extra_subs) > 0:
+        print("Skipping %s, more subscriptions" % name)
+        continue
 
     start_timestamp = subs[0][1]
     end_timestamp = _tstamp(add_month(action_date).replace(day=1, hour=23, second=0, minute=0) )
@@ -58,8 +61,8 @@ for member_id, name, sub_type, charge_day in list(con.execute(select([members.c.
         }))
     price = conf.get('price', sub_type)
     charge_day = action_date.replace(day=charge_day, hour=1, minute=0, second=0)
-    if charge_day.date() == action_date.date():
-        continue
+    #if charge_day.date() == action_date.date():
+    #    continue
     print("Adding charge of %s for %s on %s" % (price, name, charge_day))
     con.execute(pending_transactions.insert().values({
         'member_id': member_id,
